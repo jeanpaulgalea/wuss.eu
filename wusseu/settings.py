0c3641697c821af72aae712d64bac798e2958a50
Jean Paul Galea <jp@galea.se>
2014-08-06 14:23:45 +0200

    Modified template settings.
    - look for template files in main "templates" directory,
    	and in each individual app.

--- wusseu/settings.py
+++ wusseu/settings.py
@@ -84,3 +84,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from os.path import join
+TEMPLATE_DIRS = (
+    join(BASE_DIR, 'templates'),
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
