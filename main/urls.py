47b3f07f693ebbbb98c21bea8370fbb6088dc3ee
Jean Paul Galea <jp@galea.se>
2014-08-07 10:15:49 +0000

    Dynamic qr code generation.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from .views import shorten, redirect, modify
+from .views import shorten, redirect, qr, modify
 
 hash = '([a-zA-Z0-9]{3,9})'
 uuid = '([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})'
@@ -8,5 +8,6 @@ uuid = '([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]
 urlpatterns = patterns('',
     url(r'^$',                    view=shorten),
     url(r'^'+hash+'/?$',          view=redirect),
+    url(r'^'+hash+'/qr/?$',       view=qr),
     url(r'^'+hash+'/'+uuid+'/?$', view=modify),
 )
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
-from django.http import Http404
+import qrcode
+
+from cStringIO import StringIO
+
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect as _redirect
 
 from .models import Link
@@ -28,6 +32,27 @@ def shorten(request):
     return render(request, 'homepage.html', {
         'form': form,
     })
+
+
+def qr(request, hash):
+    """
+    """
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.ERROR_CORRECT_H,
+        box_size=8,
+        border=4,
+    )
+    qr.add_data("http://wuss.eu/"+hash)
+    qr.make(fit=True)
+    #qr.make()
+
+    img = qr.make_image()
+
+    io = StringIO()
+    img.save(io)
+
+    return HttpResponse(io.getvalue(), content_type='image/png')
 
 
 def redirect(request, hash):
