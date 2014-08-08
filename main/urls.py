3cb76e439226d06a7f0e9337dd1b7275f18f4a62
Jean Paul Galea <jp@galea.se>
2014-08-08 14:22:14 +0000

    DRY URLs.

--- main/urls.py
+++ main/urls.py
@@ -7,9 +7,9 @@ hash = '(?P<hash>[a-zA-Z0-9]{3,9})'
 uuid = '(?P<uuid>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})'
 
 urlpatterns = patterns('',
-    url(r'^$',                    view=shorten),
-    url(r'^'+hash+'/?$',          view=redirect),
-    url(r'^'+hash+'/qr/?$',       view=qr),
-    url(r'^'+hash+'/qr/save/?$',  qr, {'save': True} ),
-    url(r'^'+hash+'/'+uuid+'/?$', view=modify),
+    url(r'^$',                    view=shorten, name='homepage'),
+    url(r'^'+hash+'/?$',          view=redirect, name='redirect'),
+    url(r'^'+hash+'/qr/?$',       view=qr, name='display-qr-image'),
+    url(r'^'+hash+'/qr/save/?$',  qr, {'save': True}, name='download-qr-image'),
+    url(r'^'+hash+'/'+uuid+'/?$', view=modify, name='edit-link'),
 )
